7d440c981cf6de0f7d6788322aca5732c9f235ec
Litun098 <nayaklitun9@gmail.com>
2022-12-12 01:18:52 +0530

    Set up db for files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const multer = require('multer');
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const File = require('./models/File');
 
 const app = express();
 const upload = multer({dest:"uploads"});
@@ -13,8 +15,17 @@ app.get('/',(req,res)=>{
     res.render('index');
 })
 
-app.post('/upload', upload.single("file"), (req,res)=>{
-    res.send("hi");
+app.post('/upload', upload.single("file"),async (req,res)=>{
+    const fileData = {
+        path:req.file.path,
+        originalName:req.file.originalname,
+    }
+    if(req.body.password != null && req.body.password !== ""){
+        fileData.password = await bcrypt.hash(req.body.password,8);
+    } 
+    const file = await File.create(fileData);
+    console.log(file);
+    res.send(File.originalName)
 })
 
 app.listen(process.env.PORT);
